0f96f11bb45f9690bf175dc5448cbdc1e1625355
Harshit4053 <harshitgajipara204@gmail.com>
2025-03-16 11:07:00 +1000

    project setup

---
+++ src/components/Auth/Login.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Login = () => {
+    return ( 
+        <div className='flex items-center justify-center'> 
+        <div className='border-2 border-red-600'> 
+
+        </div>
+        </div>
+     );
+}
+ 
+export default Login;
