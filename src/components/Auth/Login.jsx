bda4a854409b9a32985a3c0951d9bdb42fbc1d05
Harshit4053 <harshitgajipara204@gmail.com>
2025-03-16 11:36:06 +1000

    Login Page

--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -1,10 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
+
 
 const Login = () => {
-    return ( 
-        <div className='flex items-center justify-center'> 
-        <div className='border-2 border-red-600'> 
 
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+const submitHandler = (e) => {
+    e.preventDefault()
+    console.log("Email is",email)
+    console.log("Password is",password)
+
+    setEmail("")
+    setPassword("")
+}
+
+
+    return ( 
+        <div className='flex h-screen w-screen items-center justify-center'> 
+        <div className='border-2 rounded-xl border-emerald-600 p-20'> 
+            <form 
+            onSubmit={(e) => {
+                submitHandler(e)
+            }}
+            className='flex flex-col items-center justify-center'>
+                <input 
+                value={email}
+                onChange={(e) => {
+                       setEmail(e.target.value)
+                }}  
+                required
+                className='text-black outline-none bg-transparent border-2 border-emerald-600 py-3 px-5 text-xl rounded-full
+                placeholder:text-gray-400' type="email" placeholder='Enter your email' />
+                <input 
+                 value={password}
+                 onChange={(e) => {
+                        setPassword(e.target.value)
+                 }}  
+                required 
+                className='text-black outline-none bg-transparent border-2 border-emerald-600 py-3 px-5 text-xl rounded-full mt-3
+                placeholder:text-gray-400' type="password"  placeholder='Enter password' />
+                <button className='mt-5 text-white outline-none border-none bg-emerald-600 py-3 px-5 text-xl rounded-full
+                placeholder:text-white'>Log In</button>
+            </form>
         </div>
         </div>
      );
---
+++ src/components/Dashboard/EmployeeDashboard.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const EmployeeDashboard = () => {
+    return ( 
+        <div>Employee</div>
+     );
+}
+ 
+export default EmployeeDashboard;
